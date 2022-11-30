77dccefd7037ff903c8f783432d3104aae1da313
Bruno Felipe <brunofelipework@gmail.com>
2022-11-30 20:08:32 -0300

    prettier: formatando o codigo

--- javascript.js
+++ javascript.js
@@ -1,14 +1,16 @@
-let a = 10;
-let b = 5;
+let a = 10
+let b = 5
 
-function soma(
-	a,
-	b
-) {
-	return (
-		a +
-		b
-	);
+function soma(a, b) {
+	return a + b
 }
 
-soma(a, b);
+soma(a, b)
+
+let pessoa = {
+	nome: "bruno",
+	idade: 12,
+	cargo: "engenheiro militar",
+	datanascimento: "01/02/2002",
+	mae: "fatima",
+}
